bfc85e799faf4e164fde15f566e672e9ff7aa1ee
ZroC00l <giftgabanakgosi@gmail.com>
2022-05-17 14:08:12 +0200

    products component page

--- electronix/components/Product.jsx
+++ electronix/components/Product.jsx
@@ -6,7 +6,18 @@ import { urlFor } from "../lib/client";
 const Product = ({ product: { image, name, slug, price } }) => {
   return (
     <div>
-      <Link href={`/product/${slug.current}`}></Link>
+      <Link href={`/product/${slug.current}`}>
+        <div className="product-card">
+          <img
+            src={urlFor(image && image[0])}
+            width={250}
+            height={250}
+            className="product-image"
+          />
+          <p className="product-name">{name}</p>
+          <p className="product-price">R {price}</p>
+        </div>
+      </Link>
     </div>
   );
 };

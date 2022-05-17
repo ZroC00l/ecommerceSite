3bb24d379f85f29fe77dad913d93836055ada8df
ZroC00l <giftgabanakgosi@gmail.com>
2022-05-17 07:20:18 +0200

    components

--- electronix/pages/index.js
+++ electronix/pages/index.js
@@ -1,14 +1,23 @@
 import React from "react";
+import {
+  Layout,
+  Product,
+  FooterBanner,
+  Footer,
+  Navbar,
+  HeroBanner,
+  Cart,
+} from "../components";
 
 const Home = () => {
   return (
     <>
       Hero Banner
-      <div>
+      <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div>
+      <div className="products-container">
         {["Product 1", "Product 2", "Product 3"].map((product) => product)}
       </div>
       Footer

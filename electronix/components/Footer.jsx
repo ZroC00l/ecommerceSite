d1d706801cf3d7adbc58fc3553ef577a29d0500e
ZroC00l <giftgabanakgosi@gmail.com>
2022-05-18 08:04:58 +0200

    social media links

--- electronix/components/Footer.jsx
+++ electronix/components/Footer.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import {
+  AiFillInstagram,
+  AiOutlineTwitter,
+  AiFillFacebook,
+} from "react-icons/ai";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container">
+      <p> &copy; 2022 Electronix. All rights reserved</p>
+      <p className="icons">
+        <Link href="https://www.instagram.com/">
+          <AiFillInstagram />
+        </Link>
+        <Link href="https://twitter.com/">
+          <AiOutlineTwitter />
+        </Link>
+        <Link href="https://www.facebook.com/">
+          <AiFillFacebook />
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default Footer;

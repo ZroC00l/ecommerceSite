bf4a7b981e0385aa312d4e9bc070cbc3d336aeb5
ZroC00l <giftgabanakgosi@gmail.com>
2022-05-19 07:31:05 +0200

    shopping cart component states

--- electronix/context/StateContext.js
+++ electronix/context/StateContext.js
@@ -1,10 +1,27 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, createContext, useState, useEffect } from "react";
 import { toast } from "react-hot-toast";
 
-const context = createContext();
+const Context = createContext();
 
 export const StateContext = ({ children }) => {
   //cart state
   const [showCart, setShowCart] = useState(false);
-  const [cartItems, setCartItems] = useState(false);
+  const [cartItems, setCartItems] = useState();
+
+  const [totalPrice, SetTotalPrice] = useState();
+  const [totalQuantities, setTotalQuantities] = useState();
+  const [qty, setQty] = useState(1);
+
+  return;
+  <Context.Provider
+    value={{
+      showCart,
+      cartItems,
+      totalPrice,
+      totalQuantities,
+      qty,
+    }}
+  >
+    {children}
+  </Context.Provider>;
 };

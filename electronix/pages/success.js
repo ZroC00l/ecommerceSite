3b1e3e56b16f495e56483fe1721649c2441a50c0
ZroC00l <giftgabanakgosi@gmail.com>
2022-05-26 08:05:31 +0200

    success transaction page

--- electronix/pages/success.js
+++ electronix/pages/success.js
@@ -1,19 +1,33 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { BsBagCheckFill } from "react-icons/bs";
-import { useRouter } from "next/router";
 
 import { useStateContext } from "../context/StateContext";
 
 const Success = () => {
   const { setCart, setTotalPrice, setTotalQuantities } = useStateContext();
-  const [order, SetOrder] = useState(null);
+
   return (
     <div className="success-wrapper">
       <div className="success">
         <p className="icon">
           <BsBagCheckFill />
         </p>
+        <h2>Thank You for your order</h2>
+        <p className="email-msg">
+          Check your email for a receipt and a confirmation of your order.
+        </p>
+        <p className="description">
+          If your have any questions, please contact us at
+          <a className="email" href="mailto:uncle_groovey@gmail.com">
+            uncle_groovey@gmail.com
+          </a>
+        </p>
+        <Link href="/">
+          <button type="button" className="btn" width="300px">
+            Continue Shopping
+          </button>
+        </Link>
       </div>
     </div>
   );
